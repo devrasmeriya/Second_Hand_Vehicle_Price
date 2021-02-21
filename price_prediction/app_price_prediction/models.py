c5a21bdf1e09c54fa0998fdcaf28b8293707bcdb
devendra rasmeriya <devrasmeriya1012@gmail.com>
2021-02-21 23:48:00 +0530

    anything u can write

---
+++ price_prediction/app_price_prediction/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class car_data(models.Model):
+     
+     originalPrice=models.CharField(max_length=20,default="0")
+     kmsDriven=models.CharField(max_length=20,default="0")
+     brandName=models.CharField(max_length=20,default="0")
+     fuelType=models.CharField(max_length=20,default="0")
+     pastOwners=models.CharField(max_length=20,default="0")
+     transmissionType=models.CharField(max_length=20,default="0")
+     registrationYear=models.CharField(max_length=20,default="0")
+     sellerType=models.CharField(max_length=20,default="0")
+     sellingPrice=models.CharField(max_length=20,default="0")
+
+     
+     
+     def __str__(self):
+        return self.brandName
+
+  
